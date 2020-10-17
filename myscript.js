a819c66c707fd61b164b0471149f5bca797960df
Masaki Kanai <58135006+masakikanai-wq@users.noreply.github.com>
2020-10-17 14:21:57 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -5,3 +5,36 @@ function openProfile() {
 function closeProfile() {
     document.getElementById("more").style.display = "none";
   }
+
+  
+  function jpnToEng() {
+      document.getElementById('japanese').style.display="none";
+      document.getElementById('english').style.display="block";
+    }
+    function engToJpn() {
+        document.getElementById('english').style.display="none";
+        document.getElementById('japanese').style.display="block";
+    }
+    
+    // ひとつのボタンで翻訳を実装する用の関数
+    // うまく動作できなかったので一旦保留
+    function translate() {
+        const careerText = document.getElementById("translate");
+        // 操作するテキストのidを取得する
+        const careerTextClass = careerText.getAttribute("class");
+        // idのクラスを取得
+    
+        if(careerTextClass == "jpn"){
+            careerText.classList.remove('jpn');
+            careerText.classList.add('eng');
+    
+            document.getElementById('japanese').style.display="none";
+            document.getElementById('english').style.display="block";
+        } else {
+            careerText.classList.remove('eng');
+            careerText.classList.add('jpn');
+    
+            document.getElementById('japanese').style.display="block";
+            document.getElementById('english').style.display="none";
+        }
+    }
